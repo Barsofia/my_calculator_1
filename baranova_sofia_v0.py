842a1d0bc56eefc20a749474b582eddc01065ad4
Barsofia <150126711+Barsofia@users.noreply.github.com>
2023-11-07 12:31:22 +0300

    Add files via upload

---
+++ baranova_sofia_v0.py
@@ -0,0 +1,17 @@
+x = float(input('x = '))
+y = float(input('y = '))
+c = input('c = ')
+if c == '+':
+    print(' x + y = ', x + y)
+elif c == '-':
+    print(' x - y = ', x - y)
+elif c == '*':
+    print(' x * y = ', x * y)
+elif c == '/':
+    print(' x / y = ', x / y)
+elif c == '//':
+    print(' x // y = ', x // y)
+elif c == '%':
+    print(' x % y = ', x % y)
+elif c == '**':
+    print(' x ** y = ', x ** y)
